06bf720ccc6a36a4b2f38d9863c9c27acf7a6f03
Ibnu Halim Mustofa <ibnuhalimm@gmail.com>
2021-10-06 07:52:02 +0700

    move Facade and add some annotation

---
+++ src/Facades/ThaiBulkSms.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Ibnuhalimm\LaravelThaiBulkSms\Facades;
+
+use Ibnuhalimm\LaravelThaiBulkSms\ThaiBulkSms as IbnuhalimmThaiBulkSms;
+use Illuminate\Support\Facades\Facade;
+
+/**
+ * @method static mixed send($phoneNumber, $message)
+ * @see Ibnuhalimm\LaravelThaiBulkSms\ThaiBulkSms
+ */
+class ThaiBulkSms extends Facade
+{
+    /**
+     * Get the registered name of the component.
+     *
+     * @return string
+     */
+    protected static function getFacadeAccessor()
+    {
+        return IbnuhalimmThaiBulkSms::class;
+    }
+}
